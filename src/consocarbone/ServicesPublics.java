3ee6715d3198d2b843e9302d58bc374553c6359b
picardalix <98279717+picardalix@users.noreply.github.com>
2022-12-19 19:16:23 +0100

    Update ServicesPublics.java

--- src/consocarbone/ServicesPublics.java
+++ src/consocarbone/ServicesPublics.java
@@ -1,28 +1,56 @@
 package consocarbone;
 
 //design pattern singleton 
+/**
+ * Classe service publics : poste de consommation carbone induit par les services publics.
+ * Tous les francais ont la meme empreinte pour ce poste
+ * Il a donc un design patttern singleton
+ *
+ */
 
 public final class ServicesPublics extends ConsoCarbone {
 	
+	/**
+	 * instance unique pre-initialisee
+	 */
 	private static final ServicesPublics INSTANCE = new ServicesPublics();
-	
+	/**
+	 * Constructeur par defaut de la classe
+	 */
 	private ServicesPublics() {
 	}
+	
+	/** 
+     * @return l'impact des services publics de l'utilisateur
+	 */
 	
 	@Override
 	public double getImpact() {
 		this.impact = 1.5;
 		return this.impact;
 	}
+	/**
+	 * Acces a l'instance unique du singleton 
+	 * @return l'instance
+	 */
 	
 	public static ServicesPublics getInstance() {
 		return INSTANCE;
 	}
+	 /**
+	  * affiche l'impact des services publics de cet utilisateur
+	  * @see ConsoCarbone#toString()
+	  */
 
 	@Override 
 	 public String toString() {
 		   return "\n\nL'impact de vos services publics est de : 1,5 TCO2eq\n";
-	   }
+	}
+	
+    /**
+    * Reecriture de la methode clone de la classe object pour cree un nouveau poste de service public avec les meme argument que l'objet clone
+    * @return nouvel object clone 
+    */
 	
 	  @Override
 	   public Object clone() {
