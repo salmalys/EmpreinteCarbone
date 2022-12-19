18b275a6faa1a45c21d524447794c20a8b19fd98
picardalix <98279717+picardalix@users.noreply.github.com>
2022-12-19 18:50:34 +0100

    Update CE.java

--- src/consocarbone/CE.java
+++ src/consocarbone/CE.java
@@ -8,9 +8,6 @@ package consocarbone;
 public enum CE {
     A(0.005),B(0.01),C(0.02),D(0.035),E(0.055),F(0.08),G(0.1);
 	
-	/**
-	 * Le coefficient multiplicatif dependant de la classe energetique du logement 
-	 */
 	private final double coeff;
 	
 	/**
