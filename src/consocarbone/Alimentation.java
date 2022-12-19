bd21ad86e556709e3f1c5785e3010e9323b4cf0f
picardalix <98279717+picardalix@users.noreply.github.com>
2022-12-19 18:44:45 +0100

    Update Alimentation.java

--- src/consocarbone/Alimentation.java
+++ src/consocarbone/Alimentation.java
@@ -1,95 +1,120 @@
 package consocarbone;
-/**
- * Une Alimentation est un poste de consommation qui s'interesse a l'impact alimentaire
- *
- */
-//setteurs sur txboeuf et txvege 
+				/**
+				 * Une Alimentation est un poste de consommation qui s'interesse a l'impact alimentaire
+				 *
+				 */
+				//setteurs sur txboeuf et txvege 
 
-public class Alimentation extends ConsoCarbone{
-	/**
-	 * taux de repas a base de boeuf 
-	 */
-    private double txBoeuf;
-    /**
-     * taux de repas vegetariens
-     */
-    private double txVege;
-    
-    private final double c1 =8;
-    private final double c2 =1.6;
-    private final double c3 =.9;
-    
-    /**
-     * Constructeur de la classe
-     * @param tB
-     * @param tV
-     */
-    
-    public Alimentation() {
-    }
-    
-    public Alimentation (double tB, double tV) {
-    	this.txBoeuf = tB;
-    	this.txVege = tV; 
-    }
-    
-    public double gettxBoeuf() {
-		return txBoeuf;
-	}
+				public class Alimentation extends ConsoCarbone{
+				    private double txBoeuf;
+				    private double txVege;
+				    
+				    private final double c1 =8;
+				    private final double c2 =1.6;
+				    private final double c3 =.9;
+				    
+				    /**
+				     * Constructeur par defaut de la classe
+				     * @param tB initialise a 0
+				     * @param tV initialise a 0
+				     */
+				    
+				    public Alimentation() {
+					    this.txBoeuf = 0.0;
+					    this.txVege = 0.0;
+					    
+				    }
+				    
+					 /**
+				     * Constructeur de la classe
+				     * @param tB
+				     * @param tV
+				     */
+					
+				    public Alimentation (double tB, double tV) {
+				    	this.txBoeuf = tB;
+				    	this.txVege = tV; 
+				    }
+					
+				  /**
+				   * Getter du taux de boeuf 
+				   */
+				    
+				    public double gettxBoeuf() {
+						return txBoeuf;
+					}
 
-	public void settxBoeuf(double txBoeuf) {this.txBoeuf = txBoeuf;}
-    
-    public double gettxVege() {
-		return txVege;
-	}
+					
+					  /**
+					   * Setter du taux de boeuf
+					   */
+					    
+				    
+					public void settxBoeuf(double txBoeuf) {this.txBoeuf = txBoeuf;}
+				    
+					  /**
+					   * Getter du tauxvegetarien
+					   */
+					    			
+				    public double gettxVege() {
+						return txVege;
+					}
 
-	public void settxVege(double txVege) {
-		this.txVege = txVege;
-	}
-    
-    /**
-     * obtient l'impact calcule a partir d'une formule
-     * @return impact alimentaire
-     */
-    @Override public double getImpact() {
-    	this.impact = c1 * this.txBoeuf + c2 * (1 - this.txVege - this.txBoeuf)+ c3 * this.txVege;
-    	return this.impact;
-    }
-    
-   /**
-    * affiche l'empreinte carbonne moyenne alimenatire d'un francais
-    * @see ConsoCarbone#empCarbMoy()
-    */ 
-   public static void empCarbMoy() {
-	    ConsoCarbone.empCarbMoy();
-	   
-	    String unite = "kg CO2eq/an";
-		System.out.printf("Viandes et Poissons : 1 144 %s\n", unite);
-		System.out.printf("Produits laitiers et oeufs : 408 %s\n", unite);
-		System.out.printf("Autres : 538 %s\n", unite);
-		System.out.printf("Boissons : 263 %s\n", unite);
-	}	   
-   
-   /**
-    * affiche l'impact alimenataire de cet utilisateur
-    * @see ConsoCarbone#toString()
-    */
-   @Override
-   public String toString() {
-	   return "\nL'impact de votre alimentation est de : " + Math.round(this.impact*100.0)/100.0 + " TCO2eq\n";
-   }
-   
-   @Override
-   public Object clone() {
-		try {
-			Alimentation a = (Alimentation) super.clone();
-			return a;
-		}
-		catch (CloneNotSupportedException e){
-			throw new InternalError();
-		}
-   }
-   
-   
-   
-}
+				    /**
+					   * Setter du taux vegetarien
+					   */
+ 
+					public void settxVege(double txVege) {
+						this.txVege = txVege;
+					}
+				    
+				    /**
+				     * obtient l'impact calcule a partir d'une formule
+				     * @return impact alimentaire
+				     */
+				    @Override public double getImpact() {
+				    	this.impact = c1 * this.txBoeuf + c2 * (1 - this.txVege - this.txBoeuf)+ c3 * this.txVege;
+				    	return this.impact;
+				    }
+				    
+				   /**
+				    * affiche l'empreinte carbonne moyenne alimenatire d'un francais
+				    * @see ConsoCarbone#empCarbMoy()
+				    */ 
+				   public static void empCarbMoy() {
+					    ConsoCarbone.empCarbMoy();
+					   
+					    String unite = "kg CO2eq/an";
+						System.out.printf("Viandes et Poissons : 1 144 %s\n", unite);
+						System.out.printf("Produits laitiers et oeufs : 408 %s\n", unite);
+						System.out.printf("Autres : 538 %s\n", unite);
+						System.out.printf("Boissons : 263 %s\n", unite);
+					}	   
+				   
+				   /**
+				    * affiche l'impact alimenataire de cet utilisateur
+				    * @see ConsoCarbone#toString()
+				    */
+				   @Override
+				   public String toString() {
+					   return "\nL'impact de votre alimentation est de : " + Math.round(this.impact*100.0)/100.0 + " TCO2eq\n";
+				   }
+				   
+				   /**
+				    * Reecriture de la methode clone de la classe object pour cree un nouveau poste d'alimentation avec les meme argument que l'objet clone
+				    * @return nouvel object clone 
+				    */
+				   
+				   @Override
+				   public Object clone() {
+						try {
+							Alimentation a = (Alimentation) super.clone();
+							return a;
+						}
+						catch (CloneNotSupportedException e){
+							throw new InternalError();
+						}
+				   }
+				   
+				   
+				}
