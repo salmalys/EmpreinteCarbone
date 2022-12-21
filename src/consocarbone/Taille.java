697bd91a4821f7d0585448e8e36c5b5e4d114b21
picardalix <98279717+picardalix@users.noreply.github.com>
2022-12-21 20:34:32 +0100

    Update Taille.java

--- src/consocarbone/Taille.java
+++ src/consocarbone/Taille.java
@@ -1,15 +1,24 @@
 package consocarbone;
 
+/**
+ * Enumeration Taille, qui a deux attributs P et G correspondant a une petite et grande voiture
+ */
 public enum Taille {
     P(4.2),G(19);
 	
 	private double consVoit;
 	
-	Taille(double consVoit){
-		this.consVoit = consVoit;
-	}
+	/**
+	 * Set la consommation de la voiture
+	 * @param consVoit
+	 */
 	
-	public double getConsVoit() {
-		return consVoit;
-	}
+	Taille(double consVoit){this.consVoit = consVoit;}
+	
+	/**
+	 * Getter de la consommation de la voiture
+	 * @return la consommation de la voiture de l'utilisateur
+	 */
+	
+	public double getConsVoit() {return consVoit;}
 }
