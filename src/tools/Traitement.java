0f5e3df4844d22e2cf950a746c8d59344ad2274c
picardalix <98279717+picardalix@users.noreply.github.com>
2022-12-21 19:33:23 +0100

    Update Traitement.java

--- src/tools/Traitement.java
+++ src/tools/Traitement.java
@@ -24,12 +24,13 @@ import user.Utilisateur;
  */
 
 public class Traitement {
+
 	/**
-	 * Ajout des utilisateurs pour creer la population 
-	 * @param filenames
-	 * @return
+	 * Ajout des utilisateurs pour creer la population a partir de la lecture de fichiers
+	 * @param filenames : tableau comprenant des noms de fichiers
+	 * @return la population cree
 	 */
-	
+
 	public static Population readPop(String... filenames) {
 		Population p = new Population();
 		for (String f: filenames) {
@@ -38,7 +39,7 @@ public class Traitement {
 		}
 		return p;
 	}
-	
+
 	/**
 	 * Permet d'ouvrir les fichiers donnes en arguments en ligne de commande, puis le lit ligne par ligne, et les traites une par une. 
 	 * Leve les exceptions concernant l'ouverture des fichiers
@@ -47,113 +48,113 @@ public class Traitement {
 	 */
 
 	public static Utilisateur readUser(String filename){
-	    try
-	    {
-	      File file = new File(filename);    
-	      FileReader fr = new FileReader(file);  
-	      BufferedReader br = new BufferedReader(fr);  
-	     
-	      ArrayList<ConsoCarbone> listPostes = new ArrayList<>();
-	      
-	      String line;
-	      while((line = br.readLine()) != null)
-	      {
-	    	try {
-	    		readConso(line.split(";"), listPostes);
-	    	}
-	    	catch (IllegalArgumentException e) {
-	    		e.printStackTrace();
-	    		System.exit(1);
-	    	}
-	      }   
-	      fr.close(); 
-	      Utilisateur u = new Utilisateur(listPostes);
-	      
-	      return u;
-	    }
-	    
-	    catch(FileNotFoundException e){
-	    	System.out.println("Le fichier "+filename+" est introuvable\n");
-	    	//e.printStackTrace();
-	    	System.exit(1);
-	    	}
-	    catch(IOException e){
-	    	e.printStackTrace();
-	    	System.exit(1);
-	    	}
-	    
-	    return null;
+		try
+		{
+			File file = new File(filename);    
+			FileReader fr = new FileReader(file);  
+			BufferedReader br = new BufferedReader(fr);  
+
+			ArrayList<ConsoCarbone> listPostes = new ArrayList<>();
+
+			String line;
+			while((line = br.readLine()) != null)
+			{
+				try {
+					readConso(line.split(";"), listPostes);
+				}
+				catch (IllegalArgumentException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}   
+			fr.close(); 
+			Utilisateur u = new Utilisateur(listPostes);
+
+			return u;
+		}
+
+		catch(FileNotFoundException e){
+			System.out.println("Le fichier "+filename+" est introuvable\n");
+			//e.printStackTrace();
+			System.exit(1);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		return null;
 	}
-	
+
 	/**
 	 * Traite une ligne donnee en parametre
 	 * En fonction du nom du poste, on verifie que les arguments sont coherent avec le poste, si c'est le cas, on instancie le poste
-	 * Sinon on retourne le nom du poste ou il y a une erreur
+	 * Sinon on leve une exception car on a un mauvais argument
 	 * Si le nom du poste n'est pas reconnu on renvoie une erreur
 	 * @param line la ligne a analyser
 	 * @param listPostes ajoute un poste bien instancier a cette liste
 	 * @return 
 	 */
-	
+
 	public static void readConso(String[] line, ArrayList<ConsoCarbone> listPostes) throws IllegalArgumentException {
 		switch(line[0]) {
-			case "Alimentation": {
-				if (Verif.testTaux(line[1])&&Verif.testTaux(line[2])){
-					Alimentation a = new Alimentation(Double.parseDouble(line[1]),Double.parseDouble(line[2]));
-					listPostes.add(a);
-					return;
-				}
-				break;
-			}
-			case "Logement":{
-				int nbLogement = Integer.parseInt(line[1]);
-				for (int j = 0; j<nbLogement; j++) {
-					String superficie = line[2*j+2];
-					String ce = line[2*j+3];
-					if (Verif.testNumericPositif(superficie)&&(Verif.testCe(ce))) {
-						Logement l = new Logement(Integer.parseInt(superficie),CE.valueOf(ce));
-						listPostes.add(l);
-					}
-					else throw new IllegalArgumentException("Erreur au poste Logement");
-				}
+		case "Alimentation": {
+			if (Verif.testTaux(line[1])&&Verif.testTaux(line[2])){
+				Alimentation a = new Alimentation(Double.parseDouble(line[1]),Double.parseDouble(line[2]));
+				listPostes.add(a);
 				return;
 			}
-			case "Transport":{
-				int nbTransport = Integer.parseInt(line[1]);
-				for (int j = 0; j<nbTransport; j++) {
-					String taille = line[3*j+2];
-					String kilomAnnee = line[3*j+3];
-					String Ammortissement = line[3*j+4];
-					if (Verif.testTaille(taille)&&Verif.testNumericPositif(kilomAnnee)&&Verif.testNumericPositif(Ammortissement)) {
-						Transport t = new Transport(true,Taille.valueOf(taille),Integer.parseInt(kilomAnnee), Integer.parseInt(Ammortissement));
-						listPostes.add(t);
-					}
-					else throw new IllegalArgumentException("Erreur au poste Transport");
+			break;
+		}
+		case "Logement":{
+			int nbLogement = Integer.parseInt(line[1]);
+			for (int j = 0; j<nbLogement; j++) {
+				String superficie = line[2*j+2];
+				String ce = line[2*j+3];
+				if (Verif.testNumericPositif(superficie)&&(Verif.testCe(ce))) {
+					Logement l = new Logement(Integer.parseInt(superficie),CE.valueOf(ce));
+					listPostes.add(l);
 				}
-				if (nbTransport == 0) {
-					listPostes.add(new Transport(false));
+				else throw new IllegalArgumentException("Erreur au poste Logement");
+			}
+			return;
+		}
+		case "Transport":{
+			int nbTransport = Integer.parseInt(line[1]);
+			for (int j = 0; j<nbTransport; j++) {
+				String taille = line[3*j+2];
+				String kilomAnnee = line[3*j+3];
+				String Ammortissement = line[3*j+4];
+				if (Verif.testTaille(taille)&&Verif.testNumericPositif(kilomAnnee)&&Verif.testNumericPositif(Ammortissement)) {
+					Transport t = new Transport(true,Taille.valueOf(taille),Integer.parseInt(kilomAnnee), Integer.parseInt(Ammortissement));
+					listPostes.add(t);
 				}
+				else throw new IllegalArgumentException("Erreur au poste Transport");
+			}
+			if (nbTransport == 0) {
+				listPostes.add(new Transport(false));
+			}
+			return;
+		}
+		case "BienConso": {
+			if (Verif.testNumericPositif(line[1])){
+				BienConso b = new BienConso(Integer.parseInt(line[1]));
+				listPostes.add(b);
 				return;
 			}
-			case "BienConso": {
-				if (Verif.testNumericPositif(line[1])){
-					BienConso b = new BienConso(Integer.parseInt(line[1]));
-					listPostes.add(b);
-					return;
-				}
-				break;
+			break;
+		}
+		case "Mail":{
+			if (Verif.testNumericPositif(line[1])&&Verif.testNumericPositif(line[2])){
+				Mail m = new Mail(Integer.parseInt(line[1]),Integer.parseInt(line[2]));
+				listPostes.add(m);
+				return;
 			}
-			case "Mail":{
-				if (Verif.testNumericPositif(line[1])&&Verif.testNumericPositif(line[2])){
-					Mail m = new Mail(Integer.parseInt(line[1]),Integer.parseInt(line[2]));
-					listPostes.add(m);
-					return;
-				}
-				break;
-			}
-			default: throw new IllegalArgumentException("Erreur de nom de poste");
-	}
-		
-	throw new IllegalArgumentException("Erreur au poste "+ line[0]);
+			break;
+		}
+		default: throw new IllegalArgumentException("Erreur de nom de poste");
+		}
+
+		throw new IllegalArgumentException("Erreur au poste "+ line[0]);
 	}
 }
