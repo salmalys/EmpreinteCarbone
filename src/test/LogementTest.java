516d6b367c1051bff383ec506a6c58078459e9bf
picardalix <98279717+picardalix@users.noreply.github.com>
2022-12-21 19:53:22 +0100

    Update LogementTest.java

--- src/test/LogementTest.java
+++ src/test/LogementTest.java
@@ -8,16 +8,27 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Classe de test pour la classe Logement
+ */
+
 class LogementTest {
 	private static Logement aUnderTest;
 	private static Logement bUnderTest;
 
-	
+	/**
+	 * Setter de deux postes de logement ayant respectivement 200 de superficie et A pour classe energetique, et 350 de classe C
+	 */
+
 	@BeforeEach
 	public void setUpTests() {
 		aUnderTest = new Logement(200,CE.A);
 		bUnderTest = new Logement(350,CE.C);
 	}
+
+	/**
+	 * Met a null les deux objet instancier avant chaque test
+	 */
 
 	@AfterEach
 	public void restoreAfterTests() {
@@ -25,12 +36,21 @@ class LogementTest {
 		bUnderTest = null;
 	}
 
+	/**
+	 * Test de la methode calculImpact du logement
+	 */
+
 	@Test
 	void calculImpact_logement_returnsValueImpact() {
 		double res = aUnderTest.calculImpact();
 		assertEquals(1 ,res);
 	}
-	
+
+	/**
+	 * Test des methodes set et compareTo de la classe Logement
+	 * On fait en sorte que les deux objets cree aient les memes valeurs d'attributs, pour ensuite verifier qu'ils sont egaux
+	 */
+
 	@Test
 	void setImpact_logement_returnsInt() {
 		aUnderTest.setSuperficie(350);
