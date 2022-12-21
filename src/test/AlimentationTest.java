60ba6adfe8e2e98251a7e7b9330843cd00453760
picardalix <98279717+picardalix@users.noreply.github.com>
2022-12-21 19:44:47 +0100

    Update AlimentationTest.java

--- src/test/AlimentationTest.java
+++ src/test/AlimentationTest.java
@@ -8,11 +8,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Classe permettant de tester les methodes de la classe Alimentation
+ *
+ */
+
 class AlimentationTest {
-	//pas mettre de commentaires sur la javadoc
 	private static Alimentation aUnderTest;
 	private static Alimentation bUnderTest;
 
+	/**
+	 * Set deux postes Alimentaire ayant respectivement comme taux boeuf 0.4 et 0.7 et comme taux vegetarien 0.6 et 0.1
+	 */
 	
 	@BeforeEach
 	public void setUpTests() {
@@ -20,17 +27,30 @@ class AlimentationTest {
 		bUnderTest = new Alimentation(0.7, 0.1);
 	}
 
+	/**
+	 * Met a null les objet setter au depart de chaque test
+	 */
+
 	@AfterEach
 	public void restoreAfterTests() {
 		aUnderTest = null;
-	    bUnderTest = null;
+		bUnderTest = null;
 	}
 
+	/**
+	 * Test qui calcul l'impact alimentaire du premier poste alimentaire
+	 * Verifie que la valeur vaut bien une certaine valeur calcule grace a la formule a la main
+	 */
+	
 	@Test
 	void calculImpact_alimentation_returnsValueImpact() {
 		double res = aUnderTest.calculImpact();
 		assertEquals(3.74 ,res);
 	}
+
+	/**
+	 * Test qui verifie la methode comparaTo, et compare les deux impacts alimentaires 
+	 */
 
 	@Test
 	void compareTo_alimentation_returnsInterger() {
@@ -39,5 +59,5 @@ class AlimentationTest {
 		int res = aUnderTest.compareTo(bUnderTest);
 		assertEquals(-1,res);
 	}
-	
+
 }
