e1a4a78a203babdce54704bc286de93e1023c183
picardalix <98279717+picardalix@users.noreply.github.com>
2022-12-21 19:36:10 +0100

    Update Verif.java

--- src/tools/Verif.java
+++ src/tools/Verif.java
@@ -2,45 +2,47 @@ package tools;
 import java.util.Arrays;
 
 /**
- * Classe contenant les methodes pour verifier chaque arguments des postes de consommmation
+ * Classe contenant les methodes pour verifier que chaque arguments des postes de consommmation sont appropries
  */
 
 public class Verif {
-	
+
 	/**
 	 * Verifie si str est bien une classe energetique
-	 * @param str
+	 * @param str : String a verifier
 	 * @return boolean pour savoir si le test est reussi ou non
 	 */
+
 	public static boolean testCe(String str) {
 		String[] values = {"A", "B", "C", "D", "E", "F","G"};
-		
+
 		if (Arrays.asList(values).contains(str)) 
 			return true;
-		
+
 		return false;		
 	}
+
 	/**
 	 * Verifie si str est une bien dans l'une des deux tailles
-	 * @param str
+	 * @param str : String a verifier
 	 * @return boolean pour savoir si le test est reussi ou non
 	 */
-	
+
 	public static boolean testTaille(String str) {
 		String[] values = {"P", "G"};
-		
+
 		if (Arrays.asList(values).contains(str)) 
 			return true;
-		
+
 		return false;		
 	}
-	
+
 	/**
 	 * Verifie si str est bien un entier positif
-	 * @param str
+	 * @param str : String a verifier
 	 * @return boolean pour savoir si le test est reussi ou non
 	 */
-	
+
 	public static boolean testNumericPositif(String str) {
 		Integer intValue = 0;
 		try {
@@ -53,12 +55,13 @@ public class Verif {
 		}
 		return true;	
 	}
+
 	/**
 	 * Verifie si str est un taux, donc inclut entre 0 et 1
-	 * @param str
+	 * @param str : String a verifier
 	 * @return boolean pour savoir si le test est reussi ou non
 	 */
-	
+
 	public static boolean testTaux(String str) {
 		Double val = 0.;
 		try {
